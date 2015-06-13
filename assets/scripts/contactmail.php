2c5855b08a10fe6a464e01b041ac3f6dcf4292c9
Jayson Powers <jayson.powers@gmail.com>
2015-06-13 15:22:11 -0400

    1st attempt at form emailer

---
+++ assets/scripts/contactmail.php
@@ -0,0 +1,42 @@
+<?php
+if(isset($_POST['email'])) {
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "kyle@convictioneliquids.com";
+    $email_subject = "Contact Form Lead";
+
+ 
+    $first_name = $_POST['first_name']; // required
+    //$last_name = $_POST['last_name']; // required
+    $email_from = $_POST['email']; // required    
+    //$phone = $_POST['phone']; // required
+    //$leadtype = $_POST['leadtype']; // required
+    $comments = $_POST['comments']; // required
+ 
+    
+    $email_message = "Form details below.\n\n";
+ 
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    //$email_message .= "Last Name: ".clean_string($last_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";   
+    //$email_message .= "Phone: ".clean_string($phone)."\n";
+    //$email_message .= "Lead Type: ".clean_string($leadtype)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+sleep(2);
+echo "<meta http-equiv='refresh' content=\"0; url=http://www.convictioneliquid.info/conviction/form.php\">";
+?>
+ 
+<?php
+}
+?>
