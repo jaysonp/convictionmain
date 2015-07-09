77b8573e0153c6b921856380007377fac488635c
Jayson Powers <jayson.powers@gmail.com>
2015-07-09 19:01:07 -0400

    update

---
+++ salesform/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<link href="http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext" rel="stylesheet" type="text/css">
+  <head>
+    <title>Conviction eLiquids</title>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- CSS 
+        ================================================== -->
+    <!-- Bootstrap 3-->
+    <!-- Google Fonts -->
+    <!-- Template Styles -->
+    <link href="css/style.css" rel="stylesheet">
+      </head>
+<body>
+
+  <!--Background/Wrapper-->
+  <div class="email-background">
+
+    <!--Header-->
+      <div class="pre-header">
+        Trouble viewing this email? <a href="http://convictioneliquid.info/salesform/index.php">Click Here</a>     
+      </div>
+    <br/>
+
+    <!--Container-->
+    <div class="email-container">
+      <a href="http://convictioneliquids.info">
+        <img class="banner" src="img/hero.png">
+      </a>
+      <br/>
+           <h2 style="text-align:center;">Available in 0mg, 1mg, 3mg, 6mg, and 12mg</h2>
+        <table>
+          <tr>
+            <td>Size
+              </td>
+            <td class="redit"><strong>Limited Offer</strong>
+              </td>            
+            <td>Standard
+              </td>
+            <td>Min. Retail Price
+              </td>
+          </tr>
+            <tr>
+            <td>16ML
+              </td>
+            <td class="redit"><strong>$5.25</strong>
+              </td>            
+            <td>$6.25
+              </td>
+            <td>$12.95
+              </td>
+          </tr>
+          <tr>
+            <td>30ML
+              </td>
+            <td class="redit"><strong>$10.25</strong>
+              </td>            
+            <td>$11.25
+              </td>
+            <td>$22.95
+              </td>
+          </tr>
+        </table>
+          <p style="text-align:center;font-size:.9em;padding-top:10px;">Cost per Unit, 5 Bottles Per Flavor Minimimum for Pre-Order.
+          </p>
+        <!--Two Columns-->
+      <div class="columns">
+        <div class="left">
+          <img class="small-pic" src="img/f1.jpg"/>
+        </div>
+        <div class="right">
+          </div>
+      </div>
+        <br/>
+      <div class="columns">
+        <div class="left">
+          <img class="small-pic" src="img/f2.jpg"/>
+          </div>
+        <div class="right">
+          </div>
+        </div>
+        <br/><div class="columns">
+          <div class="left">
+          <img class="small-pic" src="img/f3.jpg"/>
+          </div>
+        <div class="right">
+
+        </div>
+      </div>
+          <br/>
+        <p>Conviction eLiquids is on the road, first at World Vapor Expo Miami, and now at VCCPA15 in Pittsburgh, with Vapor Expo International in Chicago coming next. 
+          We have received an overwhelming amount of incredible feedback from everyone who has tried our three flavors; Smash&#38;Grab, Joyride and PeelOut.
+          </p>
+      
+        <h3>How Are Conviction eLiquids Made?</h3>
+          <p>Conviction eLiquids are <a href="http://www.moleculelabs.com/molecule-made/">#MOLECULEMADE</a>, which means that they are produced using GMP compliant 
+            practices in a US-Based ISO 7 Certified Clean Room. In an industry filled with products made in unsanitary, makeshift labs, 
+            <a href="http://www.moleculelabs.com/molecule-made/">#MOLECULEMADE</a> is your protection. Conviction eLiquids are <strong class="redit">PROUDLY</strong>
+            produced in this lab environment under strict manufacturing and cleanliness standards to ensure optimum <strong class="redit">taste and purity.</strong>
+              </p>
+            <ul>
+              <li><a href="http://www.iso.org/" target="_blank">ISO Certified Clean Room</a></li>
+              <li><a href="http://www.fda.gov/Drugs/DevelopmentApprovalProcess/Manufacturing/ucm169105.htm" target="_blank">GMP Compliant Practices</a></li>
+              <li><a href="http://www.aemsa.org/members" target="_blank">AEMSA Member</a></li>
+     <!--  Comeback for the blog       <li><a href="" target="_blank">USP Kosher Grade VG and PG</a></li> -->
+              <li><a href="http://www.nicselect.com/" target="_blank">NicSelect brand 99.8% pure nicotine</a></li>
+              </ul>
+          <p><a href="https://youtu.be/96DYvbolhfk"target="_blank">Molecule Labs eLiquid Production Video</a><br>
+              </p>
+          <p>We're looking forward to adding you as a retail partner and helping you increase your bottom line. 
+              </p>
+      
+      <h2 style="text-align:center; padding-top:40px;">Add Us On Social Media</h2>
+      <div class="columns">
+        <div class="small-box">
+          <a href ="https://www.facebook.com/convictioneliquids"><img src="img/facebook.png"/></a>
+          <a href ="https://twitter.com/convictionvape"><img src="img/twitter.png"/></a>
+          <a href ="https://instagram.com/convictioneliquids"><img src="img/instagram.png"/></a>
+          </div>
+        </div>
+      <br/>
+      </div>
+
+    <br/>
+    
+    <div class="footer">
+      <h1><strong class="redit">TRUE TASTE IS OUR CONVICTION</strong></h1>
+    </div>
+
+  </div>
+</body>
+
+</html>
